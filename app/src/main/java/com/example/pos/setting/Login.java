940a6082eee1cf91880f529c1cd74ca1d8652334
VANN <tvanthey@gmail.com>
2023-02-07 10:32:20 +0700

    add btnlogin

---
+++ app/src/main/java/com/example/pos/setting/Login.java
@@ -0,0 +1,31 @@
+package com.example.pos.setting;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.pos.MainActivity;
+import com.example.pos.databinding.ActivityLoginBinding;
+
+public class Login extends AppCompatActivity {
+
+    ActivityLoginBinding loginBinding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getSupportActionBar()!=null)
+            getSupportActionBar().hide();
+        loginBinding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(loginBinding.getRoot());
+
+        loginBinding.btnLogin.setOnClickListener(this::btnLogin);
+
+    }
+
+    private void btnLogin(View view) {
+        startActivity(new Intent(this, MainActivity.class));
+    }
+}
