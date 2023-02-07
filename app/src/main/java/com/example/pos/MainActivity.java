44b1440f9629eaf2adab9e743692636aeb7ba6f6
VANN <tvanthey@gmail.com>
2023-02-07 14:32:43 +0700

    add font

--- app/src/main/java/com/example/pos/MainActivity.java
+++ app/src/main/java/com/example/pos/MainActivity.java
@@ -5,13 +5,18 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.fragment.app.Fragment;
 
+import com.example.pos.category.Frag_category;
 import com.example.pos.dasboard.Frag_Dashboard;
 import com.example.pos.databinding.ActivityMainBinding;
+import com.example.pos.inventory.Frag_inventory;
+import com.example.pos.report.Frag_report;
+import com.example.pos.sale.Frag_sale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         setSupportActionBar(binding.actionBar.customActionbar);
+        setTitle("Blue Technology");
 
         drawerToggle = new ActionBarDrawerToggle(this,
                 binding.navDrawerLayout,
@@ -39,22 +45,38 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.cart:
+                Toast.makeText(this, "Shopping Cart", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
+    }
+
     private boolean NavigationSelected(MenuItem menuItem) {
         switch (menuItem.getItemId()) {
             case R.id.dashboad:
                 setStateFragement(new Frag_Dashboard());
                 break;
             case R.id.sale:
+                setStateFragement(new Frag_sale());
                 break;
             case R.id.category:
+                setStateFragement(new Frag_category());
                 break;
             case R.id.inventory:
+                setStateFragement(new Frag_inventory());
                 break;
             case R.id.report:
+                setStateFragement(new Frag_report());
                 break;
             case R.id.setting:
+                Toast.makeText(this, "Setting", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.logout:
+                Toast.makeText(this, "Logout", Toast.LENGTH_SHORT).show();
                 break;
 
         }
@@ -63,7 +85,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setStateFragement(Fragment fragment) {
-        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame_layout,fragment).commit();
+        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame_layout, fragment).commit();
     }
 
 
