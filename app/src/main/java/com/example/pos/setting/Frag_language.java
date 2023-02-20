02e553b7817f74b1b500cfa26adb06785cb00fa4
VANN <tvanthey@gmail.com>
2023-02-20 08:42:06 +0700

    - add fragment language

--- app/src/main/java/com/example/pos/setting/Frag_language.java
+++ app/src/main/java/com/example/pos/setting/Frag_language.java
@@ -1,5 +1,6 @@
 package com.example.pos.setting;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,17 +8,29 @@ import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 
+import com.example.pos.LocalHelper;
+import com.example.pos.R;
 import com.example.pos.databinding.FragmentFragLanguageBinding;
 
 public class Frag_language extends Fragment {
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         FragmentFragLanguageBinding binding;
         binding = FragmentFragLanguageBinding.inflate(getLayoutInflater(), container, false);
+        binding.radioLangGroup.setOnCheckedChangeListener((radioGroup, i) -> {
+            switch (i) {
+                case R.id.setLangToKh:
+                    LocalHelper.setLocale(requireContext(),"kh");
+                    break;
+                case R.id.setLangToEn:
+                    LocalHelper.setLocale(requireContext(),"en");
+                    break;
 
-
+            }
+        });
         return binding.getRoot();
     }
 }
--- app/src/main/java/com/example/pos/setting/Preference.java
+++ app/src/main/java/com/example/pos/setting/Preference.java
@@ -6,10 +6,13 @@ import android.graphics.Bitmap;
 import android.hardware.display.DisplayManager;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.View;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import com.example.pos.R;
 import com.example.pos.databinding.ActivityPreferenceBinding;
 
 import java.util.Locale;
@@ -31,13 +34,22 @@ public class Preference extends AppCompatActivity {
 
         binding.changeCompanyAddress.setOnClickListener(v -> Toast.makeText(this, "Company Address", Toast.LENGTH_SHORT).show());
         binding.changeCompanyName.setOnClickListener(v -> Toast.makeText(this, "Company Name", Toast.LENGTH_SHORT).show());
-        binding.changeAppLanguage.setOnClickListener(v -> Toast.makeText(this, "Language", Toast.LENGTH_SHORT).show());
+        binding.changeAppLanguage.setOnClickListener(v -> {
+            binding.layoutPreference.setVisibility(View.GONE);
+            binding.layoutFramePreference.setVisibility(View.VISIBLE);
+            OnChangeLanguage(new Frag_language());
+            Toast.makeText(this, "Language", Toast.LENGTH_SHORT).show();
+        });
         binding.changeCurrencyExchange.setOnClickListener(v -> Toast.makeText(this, "Currency Exchange", Toast.LENGTH_SHORT).show());
         binding.changeCompanyPhoneNumber.setOnClickListener(v -> Toast.makeText(this, "Phone Number", Toast.LENGTH_SHORT).show());
         binding.changeCompanyLogo.setOnClickListener(v -> Toast.makeText(this, "Company Logo", Toast.LENGTH_SHORT).show());
         binding.changePaymentMethod.setOnClickListener(v -> Toast.makeText(this, "Payment Method", Toast.LENGTH_SHORT).show());
         binding.editPrintInvoice.setOnClickListener(v -> Toast.makeText(this, "Print Invoice", Toast.LENGTH_SHORT).show());
 
+    }
+
+    private void OnChangeLanguage(Fragment fragment){
+        getSupportFragmentManager().beginTransaction().replace(R.id.layout_frame_preference,fragment).commit();
     }
 
 }
