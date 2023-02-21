50be4deb5c128d9569ba203a70356ad67bb3e6e6
VANN <tvanthey@gmail.com>
2023-02-21 13:13:01 +0700

    Add custom product item

--- app/src/main/java/com/example/pos/product/AdapterProduct.java
+++ app/src/main/java/com/example/pos/product/AdapterProduct.java
@@ -1,4 +1,36 @@
 package com.example.pos.product;
 
-public class AdapterProduct {
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import com.example.pos.Database.Entity.Item;
+
+import java.util.List;
+
+public class AdapterProduct extends BaseAdapter {
+    List<Item> itemList;
+    Context ctx;
+    @Override
+    public int getCount() {
+        return itemList.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return i;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+
+
+        return view;
+    }
 }
