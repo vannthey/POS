cc37215eb501b3a1dc9c7266320f59f6be8a9b55
VANN <tvanthey@gmail.com>
2023-02-27 14:45:39 +0700

    update

--- app/src/main/java/com/example/pos/sale/SaleTransaction.java
+++ app/src/main/java/com/example/pos/sale/SaleTransaction.java
@@ -1,13 +1,30 @@
 package com.example.pos.sale;
 
+import androidx.room.ColumnInfo;
 import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 
 @Entity
 public class SaleTransaction {
+    @PrimaryKey
+    @ColumnInfo
     int saleId;
-    String saleName;
-    int saleQty;
-    double salePrice;
+    @ColumnInfo
+    int productId;
+    @ColumnInfo
+    String productName;
+    @ColumnInfo
+    int productQty;
+    @ColumnInfo
+    int productUnitId;
+    @ColumnInfo
+    int customerId;
+    @ColumnInfo
+    double productPrice;
+    @ColumnInfo
     double saleDiscount;
+    @ColumnInfo
     double saleSubtotal;
+    @ColumnInfo
+    String userName;
 }
